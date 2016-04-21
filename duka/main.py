73707e58f47e7febbb8db8d00c43e6ba190fe0f1
Giuseppe <giuse88@gmail.com>
2016-04-21 18:52:54 +0100

    Improved help message

--- duka/main.py
+++ duka/main.py
@@ -3,19 +3,21 @@
 import argparse
 from datetime import date, timedelta
 
-from duka.core import valid_date, set_up_signals
 from duka.app import app
+from duka.core import valid_date, set_up_signals
 
 
 def main():
     parser = argparse.ArgumentParser(prog='duka', usage='%(prog)s [options]')
-    parser.add_argument('symbols', metavar='SYMBOLS', type=str, nargs='+', help='symbol list using format EURUSD EURGBP')
+    parser.add_argument('symbols', metavar='SYMBOLS', type=str, nargs='+',
+                        help='symbol list using format EURUSD EURGBP')
     parser.add_argument('-d', '--day', type=valid_date, help='specific day format YYYY-MM-DD (default today)',
                         default=date.today() - timedelta(1))
     parser.add_argument('-s', '--startdate', type=valid_date, help='start date format YYYY-MM-DD (default today)')
     parser.add_argument('-e', '--enddate', type=valid_date, help='end date format YYYY-MM-DD (default today)')
     parser.add_argument('-t', '--thread', type=int, help='number of threads (default 20)', default=5)
-    parser.add_argument('-c', '--candle', type=str, help='candle time frame. Accepted values 1M 5M 10M 15M 30M 1H 4H')
+    parser.add_argument('-c', '--candle', type=str,
+                        help='use candles instead of ticks. Accepted values 1M 5M 10M 15M 30M 1H 4H')
     args = parser.parse_args()
 
     if args.startdate is not None:
